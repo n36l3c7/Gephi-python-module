a9253e3ee3c66ad6f9ba31a66d74e31bf64bcb26
Samuele Gervasio <gervasio.samu@gmail.com>
2019-08-22 18:00:06 +0200

    Optimized "check_instance_position"
    Optimized "check_instance_position"

--- Gephi/position.py
+++ Gephi/position.py
@@ -1,14 +1,20 @@
-from Gephi.method import secure_instance
-
-
 class Position:
     def __init__(self, x, y, z):
-        self.x = secure_instance(x, "")
-        self.y = secure_instance(y, "")
-        self.z = secure_instance(z, "")
+        self.x = check_value_position(x)
+        self.y = check_value_position(y)
+        self.z = check_value_position(z)
 
     def return_to_string(self):
         return "X: " + str(self.x) + " | Y: " + str(self.y) + " | Z: " + str(self.z)
 
     def to_string(self):
         print("X: " + str(self.x) + " | Y: " + str(self.y) + " | Z: " + str(self.z))
+
+
+def check_instance_position(value):
+    if value is None:
+        return "0"
+    elif str(value).isdigit() is False:
+        return "0"
+    else:
+        return value;
