d42699904d28b5e98640abfd8bd5ccfda14210ca
Samuele Gervasio <gervasio.samu@gmail.com>
2019-08-22 15:26:18 +0200

    Add check instance attribute
    Add check_value_color to control instance attribute.
    Its check if number is between 0 to 255 and if is a number

--- Gephi/color.py
+++ Gephi/color.py
@@ -3,12 +3,21 @@ from Gephi.method import secure_instance
 
 class Color:
     def __init__(self, r, g, b):
-        self.r = secure_instance(r, "0")
-        self.g = secure_instance(g, "0")
-        self.b = secure_instance(b, "0")
+        self.r = secure_instance(check_value_color(r), "0")
+        self.g = secure_instance(check_value_color(g), "0")
+        self.b = secure_instance(check_value_color(b), "0")
 
     def return_to_string(self):
         return "R: " + str(self.r) + " | G: " + str(self.g) + " | B: " + str(self.b)
 
     def to_string(self):
         print("R: " + str(self.r) + " | G: " + str(self.g) + " | B: " + str(self.b))
+
+
+def check_value_color(value):
+    if str(value).isdigit() is False:
+        return "0"
+    elif int(value) < 0 or int(value) > 255:
+        return "0"
+    else:
+        return value
